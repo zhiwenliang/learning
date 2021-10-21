a6e28e4e591c5849e1d349cf3f65e804a11cbee8
Zhiwen Liang <zhiwen_liang@outlook.com>
2021-10-21 17:52:16 +0800

    init repo

--- algorithms-learning/leetcode-study/3-longest-substring-without-repeating-characters.py
+++ algorithms-learning/leetcode-study/3-longest-substring-without-repeating-characters.py
@@ -1,3 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        def check(start, end):
+            chars = [0] * 128
+            for i in range(start, end + 1):
+                c = s[i]
+                chars[ord(c)] += 1
+                if chars[ord(1)] > 1:
+                    return False
+                return True
         
+        n = len(s)
+
+        res = 0
+        for i in range(0):
+            for j in range(i, n):
+                if check(i,j):
+                    res = max(res, j - i + 1)
+        return res
