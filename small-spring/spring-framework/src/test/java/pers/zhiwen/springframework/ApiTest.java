24ec86eeaf66c2ca8799c00a16fb45f17ee5619a
zhiwen <zhiwen_liang@outlook.com>
2021-12-07 21:45:52 +0800

    test for bean definition,get bean,use bean

--- small-spring/spring-framework/src/test/java/pers/zhiwen/springframework/ApiTest.java
+++ small-spring/spring-framework/src/test/java/pers/zhiwen/springframework/ApiTest.java
@@ -1,4 +1,21 @@
 package pers.zhiwen.springframework;
 
+import org.junit.jupiter.api.Test;
+import per.zhiwen.springframework.bean.BeansException;
+import per.zhiwen.springframework.bean.UserService;
+import per.zhiwen.springframework.bean.factory.config.BeanDefinition;
+import per.zhiwen.springframework.bean.factory.support.DefaultListableBeanFactory;
+
 public class ApiTest {
+
+    @Test
+    public void testBean() throws BeansException {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
+        beanFactory.registerBeanDefinition("UserService", beanDefinition);
+
+        UserService userService = (UserService) beanFactory.getBean("UserService");
+        userService.queryUserInfo();
+    }
 }
